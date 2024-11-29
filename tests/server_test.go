adf8737b1454968dc00a0d3b23c909be3d66ce93
Daniil Kurganov <dk30128@gmail.com>
2024-11-29 09:51:59 +0300

    Implemented UNIT-test for server TCP workmode

--- tests/server_test.go
+++ tests/server_test.go
@@ -1,116 +1,167 @@
 package tests_test
 
-// import (
-// 	"io/ioutil"
-// 	"log"
-// 	"modbus-emulator/src/server/tcp"
-// 	"testing"
-// 	"time"
+import (
+	"io/ioutil"
+	"log"
+	"modbus-emulator/src"
+	"modbus-emulator/src/utils"
+	"testing"
+	"time"
 
-// 	"github.com/stretchr/testify/assert"
-// )
+	mc "github.com/goburrow/modbus"
+	"github.com/stretchr/testify/assert"
+)
 
-// type (
-// 	addresses struct {
-// 		start int
-// 		end   int
-// 	}
-// 	testCase struct {
-// 		delay            time.Duration
-// 		browsedRegisters map[string]addresses
-// 		expectedStates   map[string][]byte
-// 	}
-// )
+type (
+	addresses struct {
+		start    uint16
+		quantity uint16
+	}
+	registersTCP struct {
+		coils []byte
+		DI    []byte
+		HR    []byte
+		IR    []byte
+	}
+	registersRTUOverTCP struct {
+		coils []bool
+		DI    []bool
+		HR    []uint16
+		IR    []uint16
+	}
+	transactionValues[T registersTCP | registersRTUOverTCP] struct {
+		delay            time.Duration
+		browsedRegisters map[string]addresses
+		expectedStates   T
+	}
+	testCase[T registersTCP | registersRTUOverTCP] struct {
+		workMode     string
+		transactions []transactionValues[T]
+	}
+)
 
-// func TestServer(t *testing.T) {
-// 	log.SetOutput(ioutil.Discard)
-// 	testCases := []testCase{
-// 		{
-// 			delay: 501 * time.Millisecond,
-// 			browsedRegisters: map[string]addresses{
-// 				"coils": {start: 0, end: 1},
-// 				"DI":    {start: 0, end: 1},
-// 				"HR":    {start: 0, end: 1},
-// 				"IR":    {start: 0, end: 1},
-// 			},
-// 			expectedStates: map[string][]byte{
-// 				"coils": {0},
-// 				"DI":    {0},
-// 				"HR":    {0},
-// 				"IR":    {0},
-// 			},
-// 		},
-// 		{
-// 			delay: 903 * time.Millisecond,
-// 			browsedRegisters: map[string]addresses{
-// 				"coils": {start: 0, end: 1},
-// 				"DI":    {start: 16, end: 18},
-// 				"HR":    {start: 0, end: 1},
-// 				"IR":    {start: 0, end: 1},
-// 			},
-// 			expectedStates: map[string][]byte{
-// 				"coils": {0},
-// 				"DI":    {0, 0},
-// 				"HR":    {0},
-// 				"IR":    {0},
-// 			},
-// 		},
-// 		{
-// 			delay: 1100 * time.Millisecond,
-// 			browsedRegisters: map[string]addresses{
-// 				"coils": {start: 0, end: 1},
-// 				"DI":    {start: 0, end: 1},
-// 				"HR":    {start: 8, end: 9},
-// 				"IR":    {start: 0, end: 1},
-// 			},
-// 			expectedStates: map[string][]byte{
-// 				"coils": {0},
-// 				"DI":    {0},
-// 				"HR":    {0},
-// 				"IR":    {0},
-// 			},
-// 		},
-// 		{
-// 			delay: 101 * time.Millisecond,
-// 			browsedRegisters: map[string]addresses{
-// 				"coils": {start: 5, end: 10},
-// 				"DI":    {start: 0, end: 1},
-// 				"HR":    {start: 0, end: 1},
-// 				"IR":    {start: 0, end: 1},
-// 			},
-// 			expectedStates: map[string][]byte{
-// 				"coils": {1, 0, 1, 0, 0},
-// 				"DI":    {0},
-// 				"HR":    {0},
-// 				"IR":    {0},
-// 			},
-// 		},
-// 		{
-// 			delay: 3 * time.Second,
-// 			browsedRegisters: map[string]addresses{
-// 				"coils": {start: 4, end: 8},
-// 				"DI":    {start: 0, end: 1},
-// 				"HR":    {start: 0, end: 1},
-// 				"IR":    {start: 0, end: 1},
-// 			},
-// 			expectedStates: map[string][]byte{
-// 				"coils": {1, 1, 0, 1},
-// 				"DI":    {0},
-// 				"HR":    {0},
-// 				"IR":    {0},
-// 			},
-// 		},
-// 	}
-// 	tcp.ServerInit()
-// 	for _, currentTestCase := range testCases {
-// 		currentRecievedStates := map[string][]byte{
-// 			"coils": tcp.Server.Coils[currentTestCase.browsedRegisters["coils"].start:currentTestCase.browsedRegisters["coils"].end],
-// 			"DI":    tcp.Server.Coils[currentTestCase.browsedRegisters["DI"].start:currentTestCase.browsedRegisters["DI"].end],
-// 			"HR":    tcp.Server.Coils[currentTestCase.browsedRegisters["HR"].start:currentTestCase.browsedRegisters["HR"].end],
-// 			"IR":    tcp.Server.Coils[currentTestCase.browsedRegisters["IR"].start:currentTestCase.browsedRegisters["IR"].end],
-// 		}
-// 		assert.Equalf(t, currentTestCase.expectedStates, currentRecievedStates,
-// 			"Error: recieved and expected states isn't equal:\n expected: %v;\n recieved: %v",
-// 			currentTestCase.expectedStates, currentRecievedStates)
-// 	}
-// }
+func TestServerTCPMode(t *testing.T) {
+	var err error
+	log.SetOutput(ioutil.Discard)
+	directoryPath := `src/pcapng_files/tests_files`
+	testCasesTCP := testCase[registersTCP]{
+		workMode: "tcp",
+		transactions: []transactionValues[registersTCP]{
+			{
+				delay: 501 * time.Millisecond,
+				browsedRegisters: map[string]addresses{
+					"coils": {start: 0, quantity: 1},
+					"DI":    {start: 0, quantity: 1},
+					"HR":    {start: 0, quantity: 1},
+					"IR":    {start: 0, quantity: 1},
+				},
+				expectedStates: registersTCP{
+					coils: []byte{0},
+					DI:    []byte{0},
+					HR:    []byte{0, 0},
+					IR:    []byte{0},
+				},
+			},
+			{
+				delay: 903 * time.Millisecond,
+				browsedRegisters: map[string]addresses{
+					"coils": {start: 0, quantity: 1},
+					"DI":    {start: 16, quantity: 2},
+					"HR":    {start: 0, quantity: 1},
+					"IR":    {start: 0, quantity: 1},
+				},
+				expectedStates: registersTCP{
+					coils: []byte{0},
+					DI:    []byte{0},
+					HR:    []byte{0, 0},
+					IR:    []byte{0},
+				},
+			},
+			{
+				delay: 1100 * time.Millisecond,
+				browsedRegisters: map[string]addresses{
+					"coils": {start: 0, quantity: 1},
+					"DI":    {start: 0, quantity: 1},
+					"HR":    {start: 8, quantity: 1},
+					"IR":    {start: 0, quantity: 1},
+				},
+				expectedStates: registersTCP{
+					coils: []byte{0},
+					DI:    []byte{0},
+					HR:    []byte{0, 39},
+					IR:    []byte{0},
+				},
+			},
+			{
+				delay: 101 * time.Millisecond,
+				browsedRegisters: map[string]addresses{
+					"coils": {start: 5, quantity: 5},
+					"DI":    {start: 0, quantity: 1},
+					"HR":    {start: 0, quantity: 1},
+					"IR":    {start: 0, quantity: 1},
+				},
+				expectedStates: registersTCP{
+					coils: []byte{5},
+					DI:    []byte{0},
+					HR:    []byte{0, 0},
+					IR:    []byte{0},
+				},
+			},
+			{
+				delay: 3 * time.Second,
+				browsedRegisters: map[string]addresses{
+					"coils": {start: 4, quantity: 4},
+					"DI":    {start: 0, quantity: 1},
+					"HR":    {start: 0, quantity: 1},
+					"IR":    {start: 0, quantity: 1},
+				},
+				expectedStates: registersTCP{
+					coils: []byte{11},
+					DI:    []byte{0},
+					HR:    []byte{0, 0},
+					IR:    []byte{0},
+				},
+			},
+		},
+	}
+	utils.DumpDirectoryPath = directoryPath
+	utils.WorkMode = testCasesTCP.workMode
+	go src.ServerInit()
+	handler := mc.NewTCPClientHandler("localhost:1502")
+	if err = handler.Connect(); err != nil {
+		log.Fatalf("Error on handler connecting: %s\n", err)
+	}
+	defer handler.Close()
+	client := mc.NewClient(handler)
+	for _, currentTCPTestCase := range testCasesTCP.transactions {
+		var currentRecievedStates registersTCP
+		if currentRecievedStates.coils, err = client.ReadCoils(currentTCPTestCase.browsedRegisters["coils"].start,
+			currentTCPTestCase.browsedRegisters["coils"].quantity); err != nil {
+			assert.EqualErrorf(t, err, "nil",
+				"Error: recieved and expected errors isn't equal:\n expected: %s;\n recieved: %s", "nil", err,
+			)
+		}
+		if currentRecievedStates.DI, err = client.ReadDiscreteInputs(currentTCPTestCase.browsedRegisters["DI"].start,
+			currentTCPTestCase.browsedRegisters["DI"].quantity); err != nil {
+			assert.EqualErrorf(t, err, "nil",
+				"Error: recieved and expected errors isn't equal:\n expected: %s;\n recieved: %s", "nil", err,
+			)
+		}
+		if currentRecievedStates.HR, err = client.ReadHoldingRegisters(currentTCPTestCase.browsedRegisters["HR"].start,
+			currentTCPTestCase.browsedRegisters["HR"].quantity); err != nil {
+			assert.EqualErrorf(t, err, "nil",
+				"Error: recieved and expected errors isn't equal:\n expected: %s;\n recieved: %s", "nil", err,
+			)
+		}
+		if currentRecievedStates.IR, err = client.ReadDiscreteInputs(currentTCPTestCase.browsedRegisters["IR"].start,
+			currentTCPTestCase.browsedRegisters["IR"].quantity); err != nil {
+			assert.EqualErrorf(t, err, "nil",
+				"Error: recieved and expected errors isn't equal:\n expected: %s;\n recieved: %s", "nil", err,
+			)
+		}
+		assert.Equalf(t, currentTCPTestCase.expectedStates, currentRecievedStates,
+			"Error: recieved and expected states isn't equal:\n expected: %v;\n recieved: %v",
+			currentTCPTestCase.expectedStates, currentRecievedStates)
+		time.Sleep(currentTCPTestCase.delay)
+	}
+}
